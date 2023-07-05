622ee7f6586be2b72fbc46792dd1a18f5ea8178e
phunt <phungothanh784@gmail.com>
2023-07-05 14:30:06 +0700

    first commit

---
+++ BirdClinicSystems/Pages/Index.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using BirdClinicSystems.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BirdClinicSystems.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public IndexModel(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+        public List<Room> RoomList = new List<Room>();
+
+        public async Task<IActionResult> OnGet()
+        {
+            var client = _clientFactory.CreateClient("YourApiClientName");
+            var response = await client.GetAsync("api/Rooms/getAllRoom");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var responseObj = JsonConvert.DeserializeObject<dynamic>(content);
+                var dataArray = responseObj.data;
+                foreach (var item in dataArray)
+
+                {
+                    var room = new Room
+                    {
+                        id = item.id,
+                        roomNumber = item.roomNumber,
+                        roomName = item.roomName,
+                        status = item.status
+                    };
+                    RoomList.Add(room);
+                }
+                return Page();
+            }
+            else
+            {
+                // Handle the error response
+                return Content("Error occurred while calling the API.");
+            }
+        }
+
+    }
+}
---
+++ BirdClinicSystems/ViewModels/Room.cs
@@ -0,0 +1,10 @@
+﻿namespace BirdClinicSystems.ViewModels
+{
+    public class Room
+    {
+        public int id { get; set; }
+        public string roomName { get; set; }
+        public int roomNumber { get; set; }
+        public int status { get; set; }
+    }
+}
